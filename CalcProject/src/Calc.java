bc2f19f6d9f38edc8ef503372019b7c79780f64f
sersldmir <2sergmir2@gmail.com>
2023-02-25 13:26:04 +0300

    added file output

--- CalcProject/src/Calc.java
+++ CalcProject/src/Calc.java
@@ -1,47 +1,55 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calc {
 
-    public static double calcFunc(String input[]){
+    public static String calcFunc(String input[]){
         double a = 0, b = 0;
-        double res = 0;
+        String res = "";
         try{
             a = Double.parseDouble(input[0]); b = Double.parseDouble(input[2]);
         }
         catch (Exception e){
-            System.out.println("Error! Not number");
-            System.exit(0);
+            return "Error! Not number";
         }
         switch(input[1]){
-            case "+": res = a+b; break;
-            case "-": res = a-b; break;
-            case "*": res = a*b; break;
+            case "+": res = Double.toString(a+b); break;
+            case "-": res = Double.toString(a-b); break;
+            case "*": res = Double.toString(a*b); break;
             case "/":
                 if (b==0){
-                    System.out.println("Error! Division by zero");
-                    System.exit(0);
+                    return "Error! Division by zero";
                 }
                 else{
-                    res = a/b; break;
+                    res = Double.toString(a/b); break;
                 }
-                break;
-            default: System.out.println("Operation Error!"); System.exit(0);
+            default: return "Operation Error!";
         }
         return res;
     }
     public static void main(String[] args) {
         try {
             File fileObj = new File("test.txt");
+            FileWriter output = new FileWriter("output.txt");
             Scanner reader = new Scanner(fileObj);
             while (reader.hasNextLine()){
-                String input[] = reader.nextLine().split(" ");
-                System.out.println(Calc.calcFunc(input));
+                String input = reader.nextLine();
+                String calc_args[] = input.split(" ");
+                output.write(String.format("%s = %s\n", input, Calc.calcFunc(calc_args)));
+                output.flush();
             }
             reader.close();
-        } catch (FileNotFoundException e) {
+            output.close();
+        } 
+        catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
+        catch (IOException e){
+            System.out.println("Smth went wrong!");
+        }
+
     }
 }
