40f112eb97853c411404aa449ad27aa046d05094
sersldmir <2sergmir2@gmail.com>
2023-02-26 10:38:43 +0300

    first table

---
+++ Swingapps/src/Databases/FirstTable.java
@@ -0,0 +1,30 @@
+package Databases;
+
+import java.sql.*;
+
+public class FirstTable {
+
+    public static Connection con;
+    public static Statement st;
+
+    public static void Conn() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        con = DriverManager.getConnection("jdbc:sqlite:My_cats.db");
+        System.out.println("Database created");
+    }
+    public static void CreateDB() throws SQLException {
+        st = con.createStatement();
+        st.execute("CREATE TABLE if not exists types (id INTEGER PRIMARY KEY AUTOINCREMENT, type VARCHAR(100))");
+        System.out.println("Table created");
+    }
+    public static void CloseDB() throws SQLException {
+        con.close();
+        st.close();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        FirstTable.Conn();
+        FirstTable.CreateDB();
+        FirstTable.CloseDB();
+    }
+}
