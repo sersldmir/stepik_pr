db04b1ff94668d276c3ccfaa6227b916de961db4
sersldmir <2sergmir2@gmail.com>
2023-02-26 14:03:15 +0300

    schedule

---
+++ Swingapps/src/YandexPartTwo/Schedule.java
@@ -0,0 +1,46 @@
+package YandexPartTwo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Schedule {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, JSONException {
+        String searchKey = "2aac0dcc-3c17-4a10-9013-a153cd7103af";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название здания:");
+        String name = scanner.nextLine().replace(" ", "+");
+
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://search-maps.yandex.ru/v1/?type=biz&lang=ru_RU&apikey=" + searchKey + "&text=" + name)).GET().build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject jo = new JSONObject(response.body());
+        System.out.println(jo);
+        JSONArray ja = (JSONArray) jo.get("features");
+
+        System.out.println("График работы '" + name.replace("+", " ") + "':");
+
+        if (ja.length() == 0) {
+            System.out.println("404 not found");
+        } else {
+            int counter = 1;
+            for (int i=0; i<ja.length(); i++) {
+                JSONObject properties = ((JSONObject) ja.getJSONObject(i)).getJSONObject("properties").getJSONObject("CompanyMetaData");
+                String schedule = "неизвестно";
+                if (properties.has("Hours")) {
+                    JSONObject hours = properties.getJSONObject("Hours");
+                    schedule = (String) hours.get("text");
+                }
+                System.out.println(counter++ + ". По адресу: " + properties.get("address") + " - " + schedule);
+            }
+        }
+    }
+}
